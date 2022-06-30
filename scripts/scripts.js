efd19fc2c2dad771c0d72d5b529d606e541e7fea
Ilia K <karasev.ilya398@gmail.com>
2022-06-30 04:10:54 +0300

    burger 90% done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,12 @@
   const header = () => {
     const headerMobile = document.querySelector(".header-mobile");
     const headerDesktop = document.querySelector(".header-desktop");
-    console.log(headerDesktop);
+    const burgerBtn = document.querySelector('.header-mobile__burger-btn')
+    const burgerLine1 = document.querySelector('.header-mobile__burger-layer-1')
+    const burgerLine2 = document.querySelector('.header-mobile__burger-layer-2')
+    const burgerLine3 = document.querySelector('.header-mobile__burger-layer-3')
+    const burgerLine4 = document.querySelector('.header-mobile__burger-layer-4')
+    const burgerLine5 = document.querySelector('.header-mobile__burger-layer-5')
 
     let screenWidth = window.screen.width;
     let screenHeight = window.screen.height;
@@ -13,25 +18,34 @@
       const main = document.querySelector(".main");
       const header = document.querySelector(".header");
       const heightHeader = window.getComputedStyle(header).height;
-      console.log(heightHeader);
       main.style.marginTop = `${heightHeader}`;
-      console.log(main);
     };
+
     const toggleBurger = () => {
       if (screenWidth <= 1000) {
-        console.log(" меньше 1000");
         headerMobile.classList.remove("visually-hidden");
         headerDesktop.classList.add("visually-hidden");
       } else {
-        console.log(" больше 1000");
         headerMobile.classList.add("visually-hidden");
         headerDesktop.classList.remove("visually-hidden");
       }
     };
+
+    const controlHeightBurgerMenu = () => {
+      const menuBuger = document.querySelector('.header-mobile__burger-menu');
+      const preview = document.querySelector('.section-preview');
+      const heightPreview = Number(window.getComputedStyle(preview).height.split('p')[0]);
+      const header = document.querySelector('.header-mobile')
+      const heightHeader = Number(window.getComputedStyle(header).height.split('p')[0])
+      const widthBurgerMenu = Number(window.getComputedStyle(menuBuger).width.split('p')[0])
+      menuBuger.style.height = `${heightPreview + heightHeader}px`
+      menuBuger.style.left = `-${widthBurgerMenu}px`
+    }
+    
     toggleBurger();
 
     controlMAinMarginTop();
-
+    controlHeightBurgerMenu()
     window.addEventListener("resize", () => {
       controlMAinMarginTop();
       screenWidth = window.innerWidth;
@@ -41,13 +55,86 @@
       //   console.log(screenHeight);
 
       toggleBurger();
+      controlMAinMarginTop();
+      // controlHeightBurgerMenu()
     });
 
     let burgerStatus = "close";
+    
+    burgerBtn.addEventListener('click', () => {
+      if (burgerStatus == "close") {
+        console.log(5);
+        setTimeout(() => {
+          burgerLine1.classList.add('layer-1-animation')
+        }, 0);
 
-    if (burgerStatus == "close") {
-    } else {
-    }
+        setTimeout(() => {
+          burgerLine2.classList.add('layer-2-animation')
+        }, 300);
+
+        setTimeout(() => {
+          burgerLine3.classList.add('layer-3-animation')
+        }, 500);
+
+        setTimeout(() => {
+          burgerLine4.classList.add('layer-4-animation')
+        }, 700);
+
+        setTimeout(() => {
+          burgerLine5.classList.add('layer-5-animation')
+        }, 700);
+
+        const menuBuger = document.querySelector('.header-mobile__burger-menu')
+        const widthBurgerMenu = Number(window.getComputedStyle(menuBuger).width.split('p')[0])
+        console.log(widthBurgerMenu);
+        let leftMax = 0;
+        let leftCount = -widthBurgerMenu;
+        const animationBurgerMenu = setInterval(() => {
+          if (leftCount >= leftMax) {
+            clearInterval(animationBurgerMenu)
+          }
+          ++leftCount
+          menuBuger.style.left = `${leftCount}px`
+        }, 2);
+  
+        burgerStatus = 'open'
+      } else {
+        setTimeout(() => {
+          burgerLine4.classList.remove('layer-4-animation')
+        }, 0);
+
+        setTimeout(() => {
+          burgerLine5.classList.remove('layer-5-animation')
+        }, 0);
+
+        setTimeout(() => {
+          burgerLine1.classList.remove('layer-1-animation')
+        }, 700);
+
+        setTimeout(() => {
+          burgerLine2.classList.remove('layer-2-animation')
+        }, 500);
+
+        setTimeout(() => {
+          burgerLine3.classList.remove('layer-3-animation')
+        }, 300);
+
+        const menuBuger = document.querySelector('.header-mobile__burger-menu')
+        const widthBurgerMenu = Number(window.getComputedStyle(menuBuger).width.split('p')[0])
+        console.log(widthBurgerMenu);
+        let leftMax = -widthBurgerMenu;
+        let leftCount = 0;
+        const animationBurgerMenu = setInterval(() => {
+          if (leftCount <= leftMax) {
+            clearInterval(animationBurgerMenu)
+          }
+          --leftCount
+          menuBuger.style.left = `${leftCount}px`
+        }, 2);
+        burgerStatus = 'close'
+      }
+    })
+
   };
 
   const animationText = () => {
